9fd8d3e665309e2cc6ca283900e107307fa9cd02
duynvph13636 <93897038+duynvph13636@users.noreply.github.com>
2022-02-20 18:57:12 +0700

    update

--- src/components/newProducts.js
+++ src/components/newProducts.js
@@ -15,7 +15,7 @@ const NewProducts = {
               
                <a href="/products/${product.id}/detail" class="group">
                <div class="w-full aspect-w-1 aspect-h-1 bg-gray-200 rounded-lg overflow-hidden xl:aspect-w-7 xl:aspect-h-8">
-                 <img src="${product.image}" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-full h-full object-center object-cover group-hover:opacity-75">
+                 <img src="${product.image}" alt="Tall slender porcelain bottle with natural clay textured body and cork stopper." class="w-[200px] h-[250px] object-center object-cover group-hover:opacity-75">
                </div>
                <h3 class="mt-4 text-sm text-gray-700">
                 ${product.name}
