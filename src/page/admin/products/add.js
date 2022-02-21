e13f3ec5bd91145ae54daba8895cb4d79adf6227
duynvph13636 <93897038+duynvph13636@users.noreply.github.com>
2022-02-21 14:16:25 +0700

    msg add product

--- src/page/admin/products/add.js
+++ src/page/admin/products/add.js
@@ -70,6 +70,7 @@ const addProduct = {
                                 Ảnh sản phẩm
                                 </label>
                                  <input type="file" id="file-upload">
+                                 <img width="200" src="https://2.bp.blogspot.com/-muVbmju-gkA/Vir94NirTeI/AAAAAAAAT9c/VoHzHZzQmR4/s1600/placeholder-image.jpg" id="img-preview" alt="">
                         </div>
                         </div>
                         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
@@ -90,21 +91,33 @@ const addProduct = {
     },
     afterRender() {
         const formAdd = document.querySelector("#formAdd");
+        const img_preview = document.querySelector("#img-preview");
+        const img_post = document.querySelector("#file-upload");
+        const imgLink = "";
+
         const CLOUDINARY_PRESET = "ck8bz8wq";
         const CLOUDINARY_API_URL = "https://api.cloudinary.com/v1_1/fpolyduy/image/upload";
+
+        img_post.addEventListener("change", (e) => {
+            img_preview.src = URL.createObjectURL(e.target.files[0]);
+        });
+
         formAdd.addEventListener("submit", async (e) => {
             e.preventDefault();
             const file = document.querySelector("#file-upload").files[0];
-            const formData = new FormData();
-            formData.append("file", file);
-            formData.append("upload_preset", CLOUDINARY_PRESET);
-            // call api cloudinary , để upload ảnh
-            const { data } = await axios.post(CLOUDINARY_API_URL, formData, {
-                headers: {
-                    "Content-Type": "application/form-data",
-                },
-            });
-            console.log(data.url);
+            if (file) {
+                const formData = new FormData();
+                formData.append("file", file);
+                formData.append("upload_preset", CLOUDINARY_PRESET);
+                // call api cloudinary , để upload ảnh
+                const { data } = await axios.post(CLOUDINARY_API_URL, formData, {
+                    headers: {
+                        "Content-Type": "application/form-data",
+                    },
+                });
+                imgLink = data.url;
+                console.log(data.url);
+            }
 
             add({
                 catePostId: document.querySelector("#idCategory").value,
@@ -112,7 +125,7 @@ const addProduct = {
                 price: document.querySelector("#price_product").value,
                 description: document.querySelector("#description_product").value,
                 quantity: document.querySelector("#soluong_product").value,
-                image: data.url,
+                image: imgLink || "",
 
             });
             // document.location.href = "/admin/products/add";
